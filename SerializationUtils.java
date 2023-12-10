e77195441f66a535940ccee28491f0319c3b29c7
ShuMAhd <shuman@zykaika.ru>
2023-12-10 14:09:34 +0800

    Усё готово, работает.

--- SerializationUtils.java
+++ SerializationUtils.java
@@ -1,11 +1,4 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.UUID;
 
 /**
@@ -14,11 +7,12 @@ import java.util.UUID;
 public class SerializationUtils {
 
   /**
-   * Сохраняет объект в файл с уникальным именем
+   * Сохраняет объект в файл с уникальным именем.
    *
    * @param object Сериализуемый объект.
+   * @return
    */
-  public static void saveToFile(Serializable object) {
+  public static String saveToFile(Serializable object) {
     // Генерируем уникальное имя файла на основе имени класса и UUID
     String fileName = object.getClass().getName() + "_" + UUID.randomUUID().toString();
 
@@ -29,6 +23,7 @@ public class SerializationUtils {
     } catch (IOException e) {
       e.printStackTrace();
     }
+    return fileName;
   }
 
   /**
@@ -41,6 +36,7 @@ public class SerializationUtils {
     try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
       // Читаем объект из файла и возвращаем его
       Object loadedObject = ois.readObject();
+      ois.close();
       System.out.println("Объект успешно загружен из файла: " + fileName);
       deleteFile(fileName); // Удаляем файл после успешной загрузки
       return loadedObject;
@@ -49,20 +45,70 @@ public class SerializationUtils {
     } catch (IOException | ClassNotFoundException e) {
       System.err.println("Ошибка при чтении файла или десериализации объекта: " + e.getMessage());
     }
+
     return null;
   }
+
   /**
    * Удаляет файл с указанным именем.
    *
    * @param fileName Имя файла для удаления.
    */
-  private static void deleteFile(String fileName){
+  private static void deleteFile(String fileName) {
     File file = new File(fileName);
-    if (file.delete()){
+    if (file.delete()) {
       System.out.println("Файл успешно удален: " + fileName);
-    }else {
+    } else {
       System.out.println("Не удалось удалить файл: " + fileName);
     }
   }
 
+  /**
+   * Пример
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    // Пример использования
+
+
+    MyClass obj = new MyClass(42, "Hello, Serialization!");
+    String fileName = saveToFile(obj);
+
+    Object loadedObject = loadFromFile(fileName);
+    if (loadedObject instanceof MyClass) {
+      MyClass loadedObj = (MyClass) loadedObject;
+      System.out.println("Загруженный объект: " + loadedObj);
+    }
+  }
+}
+
+/**
+ * Пример класса, реализующего интерфейс Serializable.
+ */
+class MyClass implements Serializable {
+  private int someData;
+  private String someString;
+
+  /**
+   * Конструктор класса.
+   *
+   * @param someData   Целочисленное значение.
+   * @param someString Строковое значение.
+   */
+  public MyClass(int someData, String someString) {
+    this.someData = someData;
+    this.someString = someString;
+  }
+
+  /**
+   * @return Строковое представление объекта.
+   */
+  @Override
+  public String toString() {
+    return "MyClass{" +
+        "someData=" + someData +
+        ", someString='" + someString + '\'' +
+        '}';
+  }
 }
